acf444afb2f6dd4a19a1b85f7620c89fab5daa17
Feglawy <a.m9518520963@outlook.com>
2024-10-31 11:30:53 +0300

    added touch test

--- src/test/java/CLI/Commands/touchCommandTest.java
+++ src/test/java/CLI/Commands/touchCommandTest.java
@@ -1,7 +1,38 @@
 package CLI.Commands;
 
+import CLI.CLIContext;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class touchCommandTest {
+    private final String TEMP_TEST_FILE = "test_file.txt";
+    private CLIContext context;
+    private ExecuteArgs touch;
 
+    @BeforeEach
+    public void setUp() {
+        context = new CLIContext();
+        touch = new touchCommand(context);
+    }
+
+    @Test
+    public void touchTest() {
+        touch.execute(new String[]{TEMP_TEST_FILE});
+
+        File file = new File(context.getCurrentDirectory(), TEMP_TEST_FILE);
+        assertTrue(file.exists());
+    }
+
+    @AfterEach
+    public void cleanUp() {
+        File file = new File(context.getCurrentDirectory(), TEMP_TEST_FILE);
+        if (file.exists()) {
+            file.delete();
+        }
+    }
 }
