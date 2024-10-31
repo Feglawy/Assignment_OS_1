d83aea4b1fcc4a3e5f0dddfe190bbcacaae8f234
islam <islamyasser840@gmail.com>
2024-10-31 15:10:16 +0300

    rm and rmdir command tests

--- src/test/java/CLI/Commands/rmCommandTest.java
+++ src/test/java/CLI/Commands/rmCommandTest.java
@@ -1,7 +1,44 @@
 package CLI.Commands;
 
+import CLI.CLIContext;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class rmCommandTest {
+    private final String TEMP_TEST_FILE = "test_file.txt";
+    private CLIContext context;
+    private IExecuteArgs rm;
 
+    @BeforeEach
+    public void setUp() {
+        context = new CLIContext();
+        rm = new rmCommand(context);
+    }
+
+    @Test
+    public void rmTest() throws IOException {
+        File file = new File(context.getCurrentDirectory(), TEMP_TEST_FILE);
+
+        assertTrue(file.createNewFile(), "File created successfully");
+
+        assertTrue(file.exists(), "");
+
+        rm.execute(new String[]{TEMP_TEST_FILE});
+
+        assertFalse(file.exists(), "File deleted ");
+    }
+
+    @AfterEach
+    public void cleanUp() {
+        File file = new File(context.getCurrentDirectory(), TEMP_TEST_FILE);
+        if (file.exists()) {
+            file.delete();
+        }
+    }
 }
--- src/test/java/CLI/Commands/rmdirCommandTest.java
+++ src/test/java/CLI/Commands/rmdirCommandTest.java
@@ -1,7 +1,44 @@
 package CLI.Commands;
 
+import CLI.CLIContext;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class rmdirCommandTest {
+    private final String TEMP_TEST_Dir = "test_Directory";
+    private CLIContext context;
+    private IExecuteArgs rmdir;
 
+    @BeforeEach
+    public void setUp() {
+        context = new CLIContext();
+        rmdir = new rmCommand(context);
+    }
+
+    @Test
+    public void rmTest() throws IOException {
+        File dir = new File(context.getCurrentDirectory(), TEMP_TEST_Dir);
+
+        assertTrue(dir.createNewFile(), "Directory created successfully");
+
+        assertTrue(dir.exists(), "");
+
+        rmdir.execute(new String[]{TEMP_TEST_Dir});
+
+        assertFalse(dir.exists(), "Directory deleted ");
+    }
+
+    @AfterEach
+    public void cleanUp() {
+        File dir = new File(context.getCurrentDirectory(), TEMP_TEST_Dir);
+        if (dir.exists()) {
+            dir.delete();
+        }
+    }
 }
