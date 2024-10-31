af8210cb50608ebc8e4de0b203539ab5e2296810
Feglawy <a.m9518520963@outlook.com>
2024-10-31 21:11:22 +0300

    fixed mv command

--- src/main/java/CLI/Commands/mvCommand.java
+++ src/main/java/CLI/Commands/mvCommand.java
@@ -2,10 +2,10 @@ package CLI.Commands;
 
 import CLI.CLIContext;
 
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 
 public class mvCommand extends Command implements IExecuteArgs, IHelp {
     public mvCommand(CLIContext context) {
@@ -16,23 +16,32 @@ public class mvCommand extends Command implements IExecuteArgs, IHelp {
     @Override
     public void execute(String[] args) {
         if (args.length != 2) {
-            System.err.println("missing arguments: <source> <destination>");
+            System.err.println("missing arguments: <source/s> <destination>");
             return;
         }
-        Path source = Paths.get(context.getCurrentDirectory(), args[0]);
-        Path destination = Paths.get(context.getCurrentDirectory(), args[1]);
 
-        Path sourceParent = source.toAbsolutePath().getParent();
-        Path destinationParent = destination.toAbsolutePath().getParent();
+        ArrayList<Path> sources = new ArrayList<>();
+        for (int i = 0; i < args.length - 1; i++) {
+            sources.add(Paths.get(context.getCurrentDirectory(), args[i]).normalize());
+        }
+        Path destination = Paths.get(context.getCurrentDirectory(), args[args.length - 1]).normalize();
 
-        boolean renamed =  sourceParent.equals(destinationParent) ;
+        if (isMoveNotValid(sources.toArray(new Path[0]), destination)) {
+            System.err.println("Multiple file moves should have directory destination.");
+            return;
+        }
+
+        boolean renamed = isRenaming(sources.toArray(new Path[0]), destination);
 
         try {
-            Files.move(source, destination);
-            if (renamed)
-                System.out.println("Renamed " + args[0] + " to " + args[1]);
-            else
-                System.out.println("Moved " + args[0] + " to " + args[1]);
+            for (Path source : sources) {
+                Path temp = renamed ? destination : destination.resolve(source.getFileName());
+                Files.move(source, temp);
+                if (renamed)
+                    System.out.println("Renamed " + source.getFileName() + " to " + destination.getFileName());
+                else
+                    System.out.println("Moved " + source.getFileName() + " to " + destination.getFileName());
+            }
         } catch (Exception e) {
             System.err.println("Failed to move file.");
         }
@@ -41,5 +50,15 @@ public class mvCommand extends Command implements IExecuteArgs, IHelp {
     @Override
     public void help() {
         System.out.println("Usage: mv <source> <destination> - Move/rename a file or directory.");
+    }
+
+    /// returns true when the destination is not a directory and one source
+    private boolean isRenaming(Path[] source, Path destination) {
+        return source.length == 1 && !destination.toFile().isDirectory();
+    }
+
+    /// if there are multiple sources the destination must be directory
+    private boolean isMoveNotValid(Path[] sources, Path destination) {
+        return sources.length > 1 && !destination.toFile().isDirectory();
     }
 }
