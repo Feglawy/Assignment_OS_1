a0b8d538aa278f0875eff3b79afacd83f9b08656
Feglawy <a.m9518520963@outlook.com>
2024-10-31 14:59:30 +0300

    added mkdir test

--- src/test/java/CLI/Commands/mkdirCommandTest.java
+++ src/test/java/CLI/Commands/mkdirCommandTest.java
@@ -1,7 +1,43 @@
 package CLI.Commands;
 
+import CLI.CLIContext;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class mkdirCommandTest {
+    private final String TEMP_TEST_DIR = "test_dir";
+    private CLIContext context;
+    private IExecuteArgs mk;
 
+    @BeforeEach
+    public void setUp() {
+        context = new CLIContext();
+        mk = new mkdirCommand(context);
+    }
+
+    @Test
+    public void mkDirTest() {
+        mk.execute(new String[]{TEMP_TEST_DIR});
+
+        File file = new File(context.getCurrentDirectory(), TEMP_TEST_DIR);
+        assertTrue(file.exists() && file.isDirectory());
+    }
+
+    @Test
+    public void mkDirShouldNotThrow()  {
+
+    }
+
+    @AfterEach
+    public void cleanUp() {
+        File file = new File(context.getCurrentDirectory(), TEMP_TEST_DIR);
+        if (file.exists()) {
+            file.delete();
+        }
+    }
 }
